bb2b8553f7cd82e1f020dce7f3ec37b5db320397
nadyash83 <150320457+nadyash83@users.noreply.github.com>
2024-01-26 09:12:01 +1000

    замечания оправила

--- homework-2/groupby.sql
+++ homework-2/groupby.sql
@@ -3,7 +3,7 @@
 SELECT ship_city, ship_country
 FROM orders
 WHERE ship_city LIKE '%burg'
-GROUP BY ship_city, ship_country
+GROUP BY ship_city, ship_country;
 
 -- 2. из таблицы orders идентификатор заказа, идентификатор заказчика, вес и страну отгрузки. Заказ отгружен в страны, начинающиеся на 'P'. Результат отсортирован по весу (по убыванию). Вывести первые 10 записей.
 
@@ -11,20 +11,20 @@ SELECT order_id, customer_id, freight, ship_country
 FROM orders
 WHERE ship_country LIKE 'P%'
 ORDER BY freight DESC
-LIMIT 10
+LIMIT 10;
 
 -- 3. имя, фамилию и телефон сотрудников, у которых в данных отсутствует регион (см таблицу employees)
 
 SELECT last_name, first_name, home_phone
 FROM employees
-WHERE region IS NULL
+WHERE region IS NULL;
 
 -- 4. количество поставщиков (suppliers) в каждой из стран. Результат отсортировать по убыванию количества поставщиков в стране
 
 SELECT country, COUNT(*)
 FROM suppliers
 GROUP BY country
-ORDER BY COUNT(*) DESC
+ORDER BY COUNT(*) DESC;
 
 -- 5. суммарный вес заказов (в которых известен регион) по странам, но вывести только те результаты, где суммарный вес на страну больше 2750. Отсортировать по убыванию суммарного веса (см таблицу orders, колонки ship_region, ship_country, freight)
 SELECT ship_country, SUM(freight) as sum
@@ -32,7 +32,7 @@ FROM orders
 WHERE ship_region IS NOT NULL
 GROUP BY ship_country
 HAVING SUM(freight) > 2750
-ORDER BY SUM(freight) DESC
+ORDER BY SUM(freight) DESC;
 
 -- 6. страны, в которых зарегистрированы и заказчики (customers) и поставщики (suppliers) и работники (employees).
 
@@ -40,11 +40,11 @@ SELECT country FROM customers
 INTERSECT
 SELECT country FROM suppliers
 INTERSECT
-SELECT country FROM employees
+SELECT country FROM employees;
 
 -- 7. страны, в которых зарегистрированы и заказчики (customers) и поставщики (suppliers), но не зарегистрированы работники (employees).
 SELECT country FROM customers
 INTERSECT
 SELECT country FROM suppliers
 EXCEPT
-SELECT country FROM employees
+SELECT country FROM employees;
