313d7ed88dbb539f07e38bc8a58132beefca2b1f
nadyash83 <150320457+nadyash83@users.noreply.github.com>
2024-02-01 11:23:24 +1000

    Update queries.sql

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -16,7 +16,10 @@ WHERE discontinued = 0 and units_in_stock < 25 and category_name in ('Dairy Prod
 ORDER BY units_in_stock;
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
-
+SELECT customers.company_name
+FROM customers 
+LEFT JOIN orders ON customers.customer_id = orders.customer_id
+WHERE (((orders.customer_id) Is Null));
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
