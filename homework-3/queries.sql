7a81aa4030a6dc0fb218351f8bb1b2207226145c
nadyash83 <150320457+nadyash83@users.noreply.github.com>
2024-02-01 11:57:29 +1000

    Update queries.sql

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -2,6 +2,11 @@
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
 
+SELECT customers.company_name, CONCAT(first_name, ' ', last_name)
+FROM (customers INNER JOIN (orders INNER JOIN employees ON orders.employee_id = employees.employee_id) ON customers.customer_id = orders.customer_id) 
+	  INNER JOIN shippers ON orders.ship_via = shippers.shipper_id
+WHERE (((employees.city) in ('London')) AND ((customers.city) in ('London')) AND ((shippers.company_name) in ('United Package')));
+
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
@@ -16,6 +21,7 @@ WHERE discontinued = 0 and units_in_stock < 25 and category_name in ('Dairy Prod
 ORDER BY units_in_stock;
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
+
 SELECT customers.company_name
 FROM customers 
 LEFT JOIN orders ON customers.customer_id = orders.customer_id
